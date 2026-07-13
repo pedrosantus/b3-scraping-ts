3391b4a0bec4b0417ecabd81ffff5f3faae88e6d
demasiadopedro <thepedrosantos@proton.me>
2026-07-12 23:48:59 -0300

    feat: add scrapper.ts and tsconfig.json, update package.json to use ES modules

---
+++ scrapper.ts
@@ -0,0 +1,42 @@
+import { firefox } from 'playwright';
+import fs from 'node:fs/promises';
+
+async function runScraper() {
+  const browser = await firefox.launch({ headless: true });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  const url = 'https://www.b3.com.br/pt_br/produtos-e-servicos/negociacao/renda-variavel/acoes/consultas/informacoes-por-periodo/relatorios-estruturados/';
+  await page.goto(url);
+
+  await page.locator('#onetrust-accept-btn-handler').click();
+
+  const frame = page.frameLocator('#bvmf_iframe');
+
+  await frame.locator('#bkdp_loading_b3').waitFor({ state: 'hidden', timeout: 60000 });
+  await frame.locator('#docCategory').click();
+  await frame.locator('id=4').click();
+
+  
+
+  const respostaPromise = page.waitForResponse(
+    (response) => response.url().includes('sistemaswebb3-listados.b3.com.br')
+  );
+
+  await frame.locator('.b3-ga-button.btn.btn-primary').click();
+  const resposta = await respostaPromise;
+  const jsonResponse = await resposta.json();
+  const items = jsonResponse.results || [];
+  
+  console.log(`Encontrados ${items.length} itens.`);
+
+  console.log('[*] dfp selecionado');
+
+  console.log('[*] LISTA DE ITENS:');
+  console.log(items);
+
+  console.log('[*] Scraper finalizado');
+  await browser.close();
+}
+
+runScraper().catch(console.error);
